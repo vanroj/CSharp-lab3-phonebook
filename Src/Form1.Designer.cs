32076e97c615c733f5fefb286a0d120439ce5688
vanroj <vanroj5@yandex.ru>
2018-12-23 20:04:42 +0300

    It hurts

---
+++ Src/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.tbZipCode = new System.Windows.Forms.TextBox();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.bDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 420);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(461, 13);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(282, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(461, 49);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(282, 20);
+            this.tbAddress.TabIndex = 2;
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(461, 85);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(282, 20);
+            this.tbCity.TabIndex = 3;
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(461, 121);
+            this.tbState.Name = "tbState";
+            this.tbState.Size = new System.Drawing.Size(282, 20);
+            this.tbState.TabIndex = 4;
+            // 
+            // tbZipCode
+            // 
+            this.tbZipCode.Location = new System.Drawing.Point(461, 156);
+            this.tbZipCode.Name = "tbZipCode";
+            this.tbZipCode.Size = new System.Drawing.Size(282, 20);
+            this.tbZipCode.TabIndex = 5;
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(461, 191);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(282, 20);
+            this.tbNumber.TabIndex = 6;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(461, 226);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(282, 20);
+            this.tbEmail.TabIndex = 7;
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.Location = new System.Drawing.Point(200, 286);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.Size = new System.Drawing.Size(543, 110);
+            this.tbInfo.TabIndex = 8;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(392, 402);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(156, 31);
+            this.bAdd.TabIndex = 9;
+            this.bAdd.Text = "Добавить";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(200, 402);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(156, 31);
+            this.bSave.TabIndex = 10;
+            this.bSave.Text = "Сохранить";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // bDelete
+            // 
+            this.bDelete.Location = new System.Drawing.Point(587, 402);
+            this.bDelete.Name = "bDelete";
+            this.bDelete.Size = new System.Drawing.Size(156, 31);
+            this.bDelete.TabIndex = 11;
+            this.bDelete.Text = "Удалить";
+            this.bDelete.UseVisualStyleBackColor = true;
+            this.bDelete.Click += new System.EventHandler(this.bDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(316, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Имя и Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(316, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Страна";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(316, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Город";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(316, 121);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Область";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(316, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Индекс";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(316, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 20);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Номер телефона";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(316, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Email";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(196, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(242, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Дополнительная информация";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bDelete);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.tbInfo);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.tbZipCode);
+            this.Controls.Add(this.tbState);
+            this.Controls.Add(this.tbCity);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.TextBox tbState;
+        private System.Windows.Forms.TextBox tbZipCode;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Src/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class AllPeople
+        {
+            public Person[] allp { get; set; }
+        }
+
+        List<Person> people = new List<Person>();
+        string path = "C:\\Users\\Ivan\\source\\repos\\Lab3\\Lab3\\In2.json";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Person p = new Person();
+            string stringList = File.ReadAllText("C:\\Users\\Ivan\\source\\repos\\Lab3\\Lab3\\In2.json");
+            AllPeople PL = JsonConvert.DeserializeObject<AllPeople>(File.ReadAllText("C:\\Users\\Ivan\\source\\repos\\Lab3\\Lab3\\In2.json"));
+            foreach (var Person in PL.allp)
+            {
+                listBox1.Items.Add(Person.FirstName); 
+                people.Add(Person);
+            };
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            if (tbName.Text != "")
+            {
+                Person p = new Person();
+                p.FirstName = tbName.Text;
+                p.Address = tbAddress.Text;
+                p.City = tbCity.Text;
+                p.State = tbState.Text;
+                p.ZipCode = tbZipCode.Text;
+                p.Email = tbEmail.Text;
+                p.PhoneNumber = tbNumber.Text;
+                p.Info = tbInfo.Text;
+                people.Add(p);
+                listBox1.Items.Add(p.FirstName);
+                string serialized = JsonConvert.SerializeObject(people, Formatting.Indented);
+                File.WriteAllText(path, null);
+                using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                   byte[] array = System.Text.Encoding.UTF8.GetBytes(serialized);
+                   fstream.Write(array, 0, array.Length);
+                }
+                tbName.Text = "";
+                tbAddress.Text = "";
+                tbCity.Text = "";
+                tbState.Text = "";
+                tbZipCode.Text = "";
+                tbEmail.Text = "";
+                tbNumber.Text = "";
+                tbInfo.Text = "";
+            }
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                tbName.Text = people[listBox1.SelectedIndex].FirstName;
+                tbAddress.Text = people[listBox1.SelectedIndex].Address;
+                tbCity.Text = people[listBox1.SelectedIndex].City;
+                tbState.Text = people[listBox1.SelectedIndex].State;
+                tbZipCode.Text = people[listBox1.SelectedIndex].ZipCode;
+                tbEmail.Text = people[listBox1.SelectedIndex].Email;
+                tbNumber.Text = people[listBox1.SelectedIndex].PhoneNumber;
+                tbInfo.Text = people[listBox1.SelectedIndex].Info;
+            }
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                people[listBox1.SelectedIndex].FirstName = tbName.Text;
+                people[listBox1.SelectedIndex].Address = tbAddress.Text;
+                people[listBox1.SelectedIndex].City = tbCity.Text;
+                people[listBox1.SelectedIndex].State = tbState.Text;
+                people[listBox1.SelectedIndex].ZipCode = tbZipCode.Text;
+                people[listBox1.SelectedIndex].Email = tbEmail.Text;
+                people[listBox1.SelectedIndex].PhoneNumber = tbNumber.Text;
+                people[listBox1.SelectedIndex].Info = tbInfo.Text;
+                listBox1.Items[listBox1.SelectedIndex] = tbName.Text;
+                string serialized = JsonConvert.SerializeObject(people, Formatting.Indented);
+                File.WriteAllText(path, null);
+                using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    byte[] array = System.Text.Encoding.UTF8.GetBytes(serialized);
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+        }
+
+        private void bDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                int del = listBox1.SelectedIndex;
+                people.RemoveAt(del);
+                listBox1.Items.RemoveAt(del);
+                string serialized = JsonConvert.SerializeObject(people, Formatting.Indented);
+                File.WriteAllText(path, null);
+                using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    byte[] array = System.Text.Encoding.UTF8.GetBytes(serialized);
+                    fstream.Write(array, 0, array.Length);
+                }
+                tbName.Text = "";
+                tbAddress.Text = "";
+                tbCity.Text = "";
+                tbState.Text = "";
+                tbZipCode.Text = "";
+                tbEmail.Text = "";
+                tbNumber.Text = "";
+                tbInfo.Text = "";
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Info { get; set; }
+    }
+}
